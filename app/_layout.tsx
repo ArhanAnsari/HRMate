8edc92163c73205bad38183effbbdfa067bbb6d0
Arhan Ansari <arhanansari2009@gmail.com>
2026-03-31 16:06:55 +0530

    feat: Integrate Sentry for error tracking and monitoring, update package.json and add configuration files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,8 +13,28 @@ import ErrorBoundary from "@/src/components/ErrorBoundary";
 import OfflineIndicator from "@/src/components/OfflineIndicator";
 import { useAuthStore } from "@/src/state/auth.store";
 import { ActivityIndicator, View } from "react-native";
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://a3bdbcce0bbdeb0c9322f4abe4a160b2@o4508228539645952.ingest.us.sentry.io/4511138640429056',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
   const { isAuthenticated, checkAuth } = useAuthStore();
   const router = useRouter();
@@ -70,4 +90,4 @@ export default function RootLayout() {
       </ThemeProvider>
     </ErrorBoundary>
   );
-}
+});
